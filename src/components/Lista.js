a0fbf3bbe2bb875219e091d4d50e1deb55e7d753
fredkendy <fredkendy9@gmail.com>
2023-12-07 09:49:32 -0300

    integracao tela de listagem com firebase

--- src/components/Lista.js
+++ src/components/Lista.js
@@ -1,32 +1,54 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {FAB, ListItem} from '@rneui/themed';
+import database from '@react-native-firebase/database';
 
 //Avisando o componente que receberá props
 export default function Lista(props) {
+
+  const [listaContatos, setListaContatos] = React.useState([])
+
+  //executado smp que inicializa o componente (eg. construtor)
+  React.useEffect(() => {
+    {/* Exemplo retirado do Firebase */}
+    database()
+      .ref('/contatos')
+      .once('value')
+      .then(snapshot => {
+        const contatos = Object.values(snapshot.val())
+        console.log('User data: ', snapshot.val());
+        setListaContatos(contatos);
+      });
+  }, [])
+
   //Estado declarado para o uso do botão, comentada pois usamos true
   //const [visible, setVisible] = React.useState(true);
   return (
-    <View style={{ display: 'flex', flex: 1 }}>
+    <View style={{display: 'flex', flex: 1}}>
       <Text style={{fontSize: 22, fontWeight: 'bold'}}>Listar contatos</Text>
       <View style={{marginTop: 16}}>
-        <ListItem>
-          <ListItem.Content>
-            <ListItem.Title>John Doe</ListItem.Title>
-            <ListItem.Subtitle>11 9999-8888</ListItem.Subtitle>
-            <ListItem.Subtitle>johndoe@email.com</ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-
-        <ListItem>
-          <ListItem.Content>
-            <ListItem.Title>John Doe</ListItem.Title>
-            <ListItem.Subtitle>11 9999-8888</ListItem.Subtitle>
-            <ListItem.Subtitle>johndoe@email.com</ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
+        {/* Map vai pegar tudo dentro de listaContatos e vai renderizar */}
+        {
+          listaContatos.map(data =>
+          <ListItem style={{ marginTop: 10 }}>
+            <ListItem.Content>
+              <ListItem.Title>{data.nome}</ListItem.Title>
+              <ListItem.Subtitle>{data.email}</ListItem.Subtitle>
+              <ListItem.Subtitle>{data.telefone}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+          ) 
+        }
       </View>
-      <FAB visible={true} title="+" color="#1E90FF" placement="right" onPress={() => { props.click() }} />
+      <FAB
+        visible={true}
+        title="+"
+        color="#1E90FF"
+        placement="right"
+        onPress={() => {
+          props.click();
+        }}
+      />
     </View>
   );
 }
